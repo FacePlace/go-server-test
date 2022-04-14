350a2bf9ae1106eaf41febd26eff3096bf3ea25e
ZeroWorldwide <43105109+FacePlace@users.noreply.github.com>
2022-04-14 11:06:52 +0300

    Basic http server

--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
-module github.com/FacePlace/go-template
+module github.com/FacePlace/go-server-test
 
 go 1.18
--- main.go
+++ main.go
@@ -1,7 +1,29 @@
 package main
 
-import "fmt"
+import (
+	"log"
+	"net/http"
+	"time"
+)
+
+type myHandler struct {
+}
+
+func (h *myHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	toSend := []byte("<html><head></head><body>Hello</hello></html>")
+	_, err := w.Write(toSend)
+	if err != nil {
+		log.Printf("Received an error while writing HTML: %s", err)
+	}
+}
 
 func main() {
-	fmt.Println("Template")
+	myServer := &http.Server{
+		Addr:         "127.0.0.1:8080",
+		ReadTimeout:  10 * time.Second,
+		WriteTimeout: 10 * time.Second,
+		Handler:      &myHandler{},
+	}
+
+	log.Fatal(myServer.ListenAndServe())
 }
